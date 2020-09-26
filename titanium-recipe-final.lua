02a5421a7af2d5fbdfd74feafe4cc2efc80847b3
Brevven <brevingame@gmail.com>
2020-09-25 20:32:36 -0700

    support for helicopters

--- titanium-recipe-final.lua
+++ titanium-recipe-final.lua
@@ -79,6 +79,12 @@ if mods["Aircraft"] then
   util.steel_to_titanium(data.raw.recipe["jet"].expensive)
 end
 
+if mods["HelicopterRevival"] or mods["Helicopters"] then
+  util.steel_to_titanium(data.raw.recipe["heli-recipe"])
+  util.steel_to_titanium(data.raw.recipe["heli-recipe"].normal)
+  util.steel_to_titanium(data.raw.recipe["heli-recipe"].expensive)
+end
+
 -- Memory storage changes
 if data.raw.item["memory-unit"] then
   util.steel_to_titanium(data.raw.recipe["memory-unit"])
