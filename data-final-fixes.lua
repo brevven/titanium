335e26af6d66bebb89ebd665e7632af75cab0f26
Brevven <brevingame@gmail.com>
2020-11-27 23:10:21 -0800

    split out space exploration

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,5 +1,18 @@
 require("titanium-recipe-final")
+
+-- Space Exploration compatibility and flavor tweaks
+require("titanium-recipe-final-se")
+
+
+-- Module fixes, including for Krastorio
 require("titanium-recipe-modules")
+
+
+-- Deadlock's stacking and crating
 require("titanium-recipe-final-stacking")
+
+-- Various playter transport mods like Aircraft
 require("titanium-recipe-final-transport")
+
+
 require("titanium-data-final-settings")
---
+++ titanium-recipe-final-se.lua
@@ -0,0 +1,47 @@
+-- Additions for Space Exploration mod.
+local util = require("__bztitanium__.data-util");
+
+if data.raw.recipe["se-space-pipe"] then
+  -- Space Exploration space stuff
+  util.steel_to_titanium(data.raw.recipe["se-space-pipe"])
+  util.steel_to_titanium(data.raw.recipe["se-space-pipe"].normal)
+  util.steel_to_titanium(data.raw.recipe["se-space-pipe"].expensive)
+  util.steel_to_titanium(data.raw.recipe["se-space-transport-belt"])
+  util.steel_to_titanium(data.raw.recipe["se-space-transport-belt"].normal)
+  util.steel_to_titanium(data.raw.recipe["se-space-transport-belt"].expensive)
+  util.steel_to_titanium(data.raw.recipe["se-space-underground-belt"])
+  util.steel_to_titanium(data.raw.recipe["se-space-underground-belt"].normal)
+  util.steel_to_titanium(data.raw.recipe["se-space-underground-belt"].expensive)
+  util.steel_to_titanium(data.raw.recipe["se-space-splitter"])
+  util.steel_to_titanium(data.raw.recipe["se-space-splitter"].normal)
+  util.steel_to_titanium(data.raw.recipe["se-space-splitter"].expensive)
+  util.steel_to_titanium(data.raw.recipe["se-space-rail"])
+  util.steel_to_titanium(data.raw.recipe["se-space-rail"].normal)
+  util.steel_to_titanium(data.raw.recipe["se-space-rail"].expensive)
+  util.add_titanium_ingredient(1, data.raw.recipe["se-space-platform-scaffold"])
+  util.add_titanium_ingredient(1, data.raw.recipe["se-space-platform-scaffold"].normal)
+  util.add_titanium_ingredient(1, data.raw.recipe["se-space-platform-scaffold"].expensive)
+
+  -- Space Exploration alternative LDS
+  util.steel_to_titanium(data.raw.recipe["se-low-density-structure-beryllium"])
+  util.steel_to_titanium(data.raw.recipe["se-low-density-structure-beryllium"].normal)
+  util.steel_to_titanium(data.raw.recipe["se-low-density-structure-beryllium"].expensive)
+
+  -- Space Exploration buildings
+  util.add_titanium_ingredient(20, data.raw.recipe["se-condenser-turbine"])
+  util.add_titanium_ingredient(20, data.raw.recipe["se-condenser-turbine"].normal)
+  util.add_titanium_ingredient(20, data.raw.recipe["se-condenser-turbine"].expensive)
+
+  -- Organization
+  data.raw.item["titanium-plate"].subgroup = "plates"
+
+  -- deadlock loaders for SE -- mods["deadlock-beltboxes-loaders"]
+  if mods["Deadlock-SE-bridge"] then
+    util.steel_to_titanium(data.raw.recipe["se-space-transport-belt-loader"])
+    util.steel_to_titanium(data.raw.recipe["se-space-transport-belt-loader"].normal)
+    util.steel_to_titanium(data.raw.recipe["se-space-transport-belt-loader"].expensive)
+    util.steel_to_titanium(data.raw.recipe["se-space-transport-belt-beltbox"])
+    util.steel_to_titanium(data.raw.recipe["se-space-transport-belt-beltbox"].normal)
+    util.steel_to_titanium(data.raw.recipe["se-space-transport-belt-beltbox"].expensive)
+  end
+end
