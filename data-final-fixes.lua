b254299a9f5a1427495baa9f261efde951d428f0
Brevven <brevingame@gmail.com>
2020-11-27 23:14:24 -0800

    split out K2 stuff

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -3,6 +3,8 @@ require("titanium-recipe-final")
 -- Space Exploration compatibility and flavor tweaks
 require("titanium-recipe-final-se")
 
+-- Krastorio 2 final fixes
+require("titanium-recipe-final-k2")
 
 -- Module fixes, including for Krastorio 2
 require("titanium-recipe-modules")
--- titanium-recipe-final.lua
+++ titanium-recipe-final.lua
@@ -14,32 +14,6 @@ util.steel_to_titanium(data.raw.recipe["flying-robot-frame"].expensive)
 
 util.add_titanium_prerequisite(data.raw.technology["robotics"])
 
--- Krastorio 2 changes
-if mods["Krastorio2"] then
-  -- Titanium modifies flying robot frames, so use them in a reasonable tech card in Krastorio 2
-  util.replace_ingredient(data.raw.recipe["advanced-tech-card"], "electric-engine-unit", "flying-robot-frame")
-
-  -- Flavor changes
-  util.rare_to_titanium(data.raw.recipe["kr-electric-mining-drill-mk2"])
-  util.rare_to_titanium(data.raw.recipe["kr-advanced-transport-belt"])
-  util.rare_to_titanium(data.raw.recipe["kr-advanced-loader"])
-  if mods["deadlock-beltboxes-loaders"] then
-    util.rare_to_titanium(data.raw.recipe["kr-advanced-transport-belt-beltbox"])
-    util.rare_to_titanium(data.raw.recipe["kr-advanced-transport-belt-loader"])
-  end
-
-  util.steel_to_titanium(data.raw.recipe["kr-quarry-drill"])
-  util.steel_to_titanium(data.raw.recipe["kr-singularity-lab"])
-
-  util.steel_to_titanium(data.raw.recipe["stack-inserter"])
-  util.steel_to_titanium(data.raw.recipe["stack-inserter"].normal)
-  util.steel_to_titanium(data.raw.recipe["stack-inserter"].expensive)
-  util.steel_to_titanium(data.raw.recipe["stack-filter-inserter"])
-  util.steel_to_titanium(data.raw.recipe["stack-filter-inserter"].normal)
-  util.steel_to_titanium(data.raw.recipe["stack-filter-inserter"].expensive)
-
-  util.add_titanium_ingredient(40, data.raw.recipe["kr-advanced-steam-turbine"])
-end
 
 -- Memory storage changes
 if data.raw.item["memory-unit"] then
