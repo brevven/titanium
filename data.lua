e2eec8d6d7c6fee9fc5e48f20991963366f2b466
Brevven <brevingame@gmail.com>
2020-11-30 07:10:58 -0800

    vulcanite smelting

--- data.lua
+++ data.lua
@@ -1,5 +1,6 @@
 require("titanium-ore")
 require("titanium-ore-particle")
 require("titanium-recipe")
-require("titanium-enriched")
+require("titanium-enriched")   -- Enriched Ti for Krastorio 2
+require("titanium-recipe-se")  -- Space Exploration special recipes (depends on K2 if present)
 
---
+++ titanium-recipe-se.lua
@@ -0,0 +1,55 @@
+-- Additional recipes if Space Exploration mod is enabled
+local util = require("__bztitanium__.data-util");
+
+if mods["space-exploration"] then
+  if mods["Krastorio2"] then
+    data:extend({
+    {
+      type = "recipe",
+      name = "enriched-titanium-smelting-vulcanite",
+      category = "smelting",
+      order = "d[titanium-plate]",
+      energy_required = 24,
+      enabled = false,
+      always_show_made_in = true,
+      allow_as_intermediate = false,
+      ingredients = {
+        {name = "enriched-titanium", amount = 8},
+        {name = "se-vulcanite-block", amount = 1},
+      },
+      results = {
+        {name = util.titanium_plate, amount = 6},
+      },
+      
+    },
+    })
+    table.insert(data.raw.technology["se-processing-vulcanite"].effects, 
+        {type = "unlock-recipe", recipe= "enriched-titanium-smelting-vulcanite"})
+    table.insert(data.raw.technology["se-processing-vulcanite"].prerequisites, "enriched-titanium")
+    data.raw.recipe["enriched-titanium-plate"].order= "d[titanium-plate]"
+  else
+    data:extend({
+    {
+      type = "recipe",
+      name = "titanium-smelting-vulcanite",
+      category = "smelting",
+      order = "d[titanium-plate]",
+      energy_required = 48,
+      enabled = false,
+      always_show_made_in = true,
+      allow_as_intermediate = false,
+      ingredients = {
+        {name = "titanium-ore", amount = 20},
+        {name = "se-vulcanite-block", amount = 1},
+      },
+      results = {
+        {name = util.titanium_plate, amount = 6},
+      },
+      
+    },
+    })
+    table.insert(data.raw.technology["se-processing-vulcanite"].effects, 
+        {type = "unlock-recipe", recipe= "titanium-smelting-vulcanite"})
+    util.add_titanium_prerequisite(data.raw.technology["se-processing-vulcanite"])
+  end
+end
