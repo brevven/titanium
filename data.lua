998221eef2c7a490d89c337848a67a892eb903ff
Brevven <brevingame@gmail.com>
2021-03-09 03:30:25 -0800

    SimpleCompress support

--- data.lua
+++ data.lua
@@ -3,4 +3,5 @@ require("titanium-ore-particle")
 require("titanium-recipe")
 require("titanium-enriched")   -- Enriched Ti for Krastorio 2
 require("titanium-recipe-se")  -- Space Exploration special recipes (depends on K2 if present)
+require("titanium-compressed")
 
