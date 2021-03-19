0fd8833a2ddb1c4cda941810baec345c8297935a
Brevven <brevingame@gmail.com>
2021-03-18 19:24:11 -0700

    py compat

--- map-gen-preset-updates.lua
+++ map-gen-preset-updates.lua
@@ -1,3 +1,4 @@
+if (not mods["pyrawores"] and not mods["bobplates"] and not mods["angelssmelting"]) then
 if data.raw["map-gen-presets"] and data.raw["map-gen-presets"].default then
   for name, preset in pairs(data.raw["map-gen-presets"].default) do 
     if type(preset) == "table" and 
@@ -7,4 +8,5 @@ if data.raw["map-gen-presets"] and data.raw["map-gen-presets"].default then
         preset.basic_settings.autoplace_controls["titanium-ore"] = preset.basic_settings.autoplace_controls["iron-ore"]
     end
   end
+end
 end
