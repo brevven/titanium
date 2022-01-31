805a87f3c106a20e91bc3685043cb9d22fc5aebc
Brevven <brevingame@gmail.com>
2022-01-30 16:17:00 -0800

    k2 fluid amount

--- titanium-enriched.lua
+++ titanium-enriched.lua
@@ -147,7 +147,7 @@ data:extend(
 		},
 		results =
 		{
-			{type = "fluid", name = "water", amount = 100, catalyst_amount = 100},
+			{type = "fluid", name = "water", amount = 90, catalyst_amount = 90},
 			{type = "item",  name = "stone", probability = 0.30, amount = 1},
 			{type = "item",  name = "titanium-ore", probability = 0.05, amount = 1}
 		},
