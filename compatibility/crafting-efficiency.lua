2bbb00146833d99ec1818164b93a7b9f4f59de51
Brevven <brevingame@gmail.com>
2023-01-16 19:03:34 -0800

    update

--- compatibility/crafting-efficiency.lua
+++ compatibility/crafting-efficiency.lua
@@ -7,10 +7,12 @@ if mods["crafting-efficiency-2"] then
 
   for i, recipe in pairs(util.me.recipes) do
     -- add remaining recipes with direct items to find the icons
-    if data.raw.recipe[recipe] and data.raw.item[recipe] and not eff_data[recipe] and util.get_result_count(recipe) == 1 then
+    if data.raw.recipe[recipe] and (data.raw.item[recipe] or data.raw.fluid[recipe])
+    and not eff_data[recipe] and 
+    (data.raw.recipe[recipe].main_product or util.get_result_count(recipe) == 1) then
       eff_data[recipe] = {
         max = 15,
-        icon =  data.raw.item[recipe].icon,
+        icon = data.raw.item[recipe] and data.raw.item[recipe].icon or data.raw.fluid[recipe].icon,
         crafting = { efficiency = 10 },
         research = { level = 6 },
       }
