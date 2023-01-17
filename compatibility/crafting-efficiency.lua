4f72f53db1e09c244f2a3ca9ee6c012946ebcf53
Brevven <brevingame@gmail.com>
2023-01-16 22:51:09 -0800

    fix

--- compatibility/crafting-efficiency.lua
+++ compatibility/crafting-efficiency.lua
@@ -9,10 +9,11 @@ if mods["crafting-efficiency-2"] then
     -- add remaining recipes with direct items to find the icons
     if data.raw.recipe[recipe] and (data.raw.item[recipe] or data.raw.fluid[recipe])
     and not eff_data[recipe] and 
+    recipe ~= "alumina" and -- for some reason this does not work, despite having a main_product
     (data.raw.recipe[recipe].main_product or util.get_result_count(recipe) == 1) then
       eff_data[recipe] = {
         max = 15,
-        icon = data.raw.item[recipe] and data.raw.item[recipe].icon or data.raw.fluid[recipe].icon,
+        icon = util.get_item_or_fluid_icon(recipe),
         crafting = { efficiency = 10 },
         research = { level = 6 },
       }
