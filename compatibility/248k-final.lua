546d1932534b5d05c986a79614df3475db9942ff
Brevven <brevingame@gmail.com>
2023-12-22 12:21:04 -0800

    248k fixes

--- compatibility/248k-final.lua
+++ compatibility/248k-final.lua
@@ -1,16 +1,18 @@
 local util = require("data-util");
 
-local ti2 = "fi_materials_titan"
+if mods["248k"] then
+  local ti2 = "fi_materials_titan"
 
--- Swap out all 248k titanium for BZ titanium_plate
-for i, recipe in pairs(data.raw.recipe) do
-  util.replace_ingredient(recipe.name, ti2, util.titanium_plate)
-  util.replace_product(recipe.name, ti2, util.titanium_plate)
+  -- Swap out all 248k titanium for BZ titanium_plate
+  for i, recipe in pairs(data.raw.recipe) do
+    util.replace_ingredient(recipe.name, ti2, util.titanium_plate)
+    util.replace_product(recipe.name, ti2, util.titanium_plate)
+  end
+
+  if mods.LootingRemnants and data.raw.recipe.gr_white_hole_cycle_fi_materials_titan_recipe~=nil then
+    data.raw.recipe.gr_white_hole_cycle_fi_materials_titan_recipe.exception_mods = {"Deconstruction", "LootingRemnants"}
+  end
+
+  -- Remove 248k titanium plate
+  util.remove_raw("item", ti2)
 end
-
-if mods.LootingRemnants and data.raw.recipe.gr_white_hole_cycle_fi_materials_titan_recipe~=nil then
-  data.raw.recipe.gr_white_hole_cycle_fi_materials_titan_recipe.exception_mods = {"Deconstruction", "LootingRemnants"}
-end
-
--- Remove 248k titanium plate
-util.remove_raw("item", ti2)
