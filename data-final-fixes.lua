3c82b2961756fb81d577b730c257b701e11c9668
Brevven <brevingame@gmail.com>
2024-12-24 02:40:24 -0800

    whitespace

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,47 +1,30 @@
 require("titanium-recipe-final")
 
 
-
 -- Space Exploration compatibility and flavor tweaks
-
 require("titanium-recipe-final-se")
 
-
-
 -- Krastorio 2 final fixes
-
 require("titanium-recipe-final-k2")
 
 
-
 -- Rich Rocks Requiem for Krastorio 2 final fixes
-
 require("titanium-recipe-final-rrr")
 
 
-
 -- Module fixes, including for Krastorio 2
-
 require("titanium-recipe-modules")
 
 
-
-
-
 -- Deadlock's stacking and crating
-
 require("titanium-recipe-final-stacking")
 
 
-
 -- 5Dim's final fixes
-
 require("titanium-recipe-final-5d")
 
 
-
 -- Modmash final fixes
-
 require("titanium-recipe-final-modmash")
 
 
