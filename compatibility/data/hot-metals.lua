505550281ffb3ce2dafc0ca44f11986611b5b575
Brevven <brevingame@gmail.com>
2025-01-01 22:48:46 -0800

    support hot metals

---
+++ compatibility/data/hot-metals.lua
@@ -0,0 +1,7 @@
+local util = require("data-util")
+util.add_hot_metals({
+  {name="titanium-plate", spoilTicks=30*60, icons={
+    { icon = "__bztitanium__/graphics/icons/titanium-plate.png", icon_size = 64},
+    { icon = "__bztitanium__/graphics/icons/titanium-plate.png", icon_size = 64, tint={.9,.3,0, .5}},
+  }},
+})
--- data.lua
+++ data.lua
@@ -5,6 +5,7 @@ require("titanium-enriched")   -- Enriched Ti for Krastorio 2
 require("titanium-recipe-se")  -- Space Exploration special recipes (depends on K2 if present)
 require("titanium-compressed")
 require("titanium-data-settings")
+require("compatibility.data.hot-metals")
 
 -- First part of hack for endgame combat
 require("compatibility/titanium-endgame-combat-data")
