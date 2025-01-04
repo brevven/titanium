5a23bef308b820e3caaf1f2e1d7550a5b7c49a2c
Brevven <brevingame@gmail.com>
2025-01-04 04:46:10 -0800

    hot metals default

--- compatibility/data/hot-metals.lua
+++ compatibility/data/hot-metals.lua
@@ -1,6 +1,6 @@
 local util = require("data-util")
 util.add_hot_metals({
-  {name="titanium-plate", spoilTicks=30*60, icons={
+  {name="titanium-plate", icons={
     { icon = "__bztitanium__/graphics/icons/titanium-plate.png", icon_size = 64},
     { icon = "__bztitanium__/graphics/icons/titanium-plate.png", icon_size = 64, tint={.9,.3,0, .5}},
   }},
