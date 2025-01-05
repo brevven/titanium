88d7f90a5698324b59bf0eed30da56716c30e5cd
Brevven <brevingame@gmail.com>
2025-01-05 03:19:14 -0800

    soft lock any planet start fix

--- data-updates.lua
+++ data-updates.lua
@@ -9,3 +9,11 @@ require("compatibility/crafting-efficiency")
 
 local util = require("data-util")
 util.redo_recycling()
+
+if mods["any-planet-start"] then
+  util.set_tech_recipe("titanium-processing", {
+    {"automation-science-pack", 1},
+    {"logistic-science-pack", 1},
+    {"chemical-science-pack", 1},
+  })
+end
