16ab799dfb8a44c8c7c13115a15cc248b89cd7ba
Brevven <brevingame@gmail.com>
2025-01-24 16:22:42 -0800

    asteroid mining

--- data-updates.lua
+++ data-updates.lua
@@ -10,6 +10,10 @@ require("compatibility/crafting-efficiency")
 local util = require("data-util")
 util.redo_recycling()
 
+if mods.Asteroid_Mining and not data.raw.item["asteroid-titanium-ore"] then
+  util.addtype("titanium-ore", {a = 0,r = 0.65,g = 0.80,b = 0.80})
+end
+
 if mods["any-planet-start"] then
   util.set_tech_recipe("titanium-processing", {
     {"automation-science-pack", 1},
